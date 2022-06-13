d87fbd8862701271438af28008e2d3410622254a
Fabio Darcy <fabio.darcy@ibm.com>
2022-06-13 20:16:57 -0300

    modificando de tsx para js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+import Lista from './componentes/Lista';
+import './css/index.css';
+
+ReactDOM.render(
+    <Lista></Lista>,
+  document.getElementById('root')
+);
